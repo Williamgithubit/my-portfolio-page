fc6d82ff353b3141335d4dfaa4eaba846ce87ebb
William T. Johnson Jr <williamj2001tealo@gmail.com>
2022-11-16 12:10:53 +0000

    update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import AuthorCard from './components/AuthorCard';
 import Education from './components/EducationCard';
+import Hobbies from './components/Hobbies';
+import Work from './components/Work'
 
 function App() {
   return (
     <>
-     <Navbar />
-     
-    
+      <Navbar />
 
       <div class="py-6">
         <main>
@@ -19,35 +19,12 @@ function App() {
 
             <AuthorCard />
 
-            {/* <!-- Work --> */}
-            <div id="work" class="bg-[#B2B2B2] h-auto my-6 py-8 rounded-lg border-4">
-              <div class="flex justify-center">
-                <h3 class="border-4 w-20  border-dashed px-4 py-4">Work</h3>
-              </div>
+            <Work />
 
-              <details class="px-6 py-4">
-                <summary class="font-bold text-2xl text-gray-800">Kwagei Group</summary>
-                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
-                  pavilions, award-winning fireworks and seasonal special events.</p>
-              </details>
-
-              <details class="px-6 py-4">
-                <summary class="font-bold text-2xl text-gray-800">EasyTech</summary>
-                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
-                  pavilions, award-winning fireworks and seasonal special events.</p>
-              </details>
-
-              <details class="px-6 py-4">
-                <summary class="font-bold text-2xl text-gray-800">Epcot Center</summary>
-                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
-                  pavilions, award-winning fireworks and seasonal special events.</p>
-              </details>
-
-            </div>
             <Education />
 
-            {/* <!-- Hobbies --> */}
-            <div id="hobbie" class="bg-[#B2B2B2] h-50 my-6 py-8 rounded-lg border-4"></div>
+            <Hobbies />
+
           </div>
           {/* <!-- /End replace --> */}
         </main>
